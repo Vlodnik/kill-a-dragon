29cd0410075f34e2a13aa96f5c8bdd843765de32
Evan Thompson <evan@studiosimpati.co>
2022-04-13 23:05:12 -0400

    restart button, flesh out basic functions, minor room additions

--- js/script.js
+++ js/script.js
@@ -8,28 +8,26 @@ let inventory = {
 	armor: {},
 	weapon: {},
 	curses: {},
-}
+};
 
 let stats = {
 	health: 10,
-	body: 1,
-	mind: 1,
-}
+	body: 0,
+	mind: 0,
+};
 
-let bookmark = {
-
-}
+let bookmark = {};
 
 $(document).ready(function() {
+
+	// Moving between rooms
 
 	let heading = $('h1');
 	let copy = $('#copy-container');
 	let options = $('#options-container');
 	
 	function moveRooms(newRoom) {
-		// if(newRoom !== startScreen) {
-		// 	heading.removeClass('caps');
-		// }
+		heading.removeClass('caps');
 
 		heading.text(newRoom.heading);
 		copy.html('');
@@ -39,15 +37,58 @@ $(document).ready(function() {
 			copy.append(`<p>${para}</p>`);
 		});
 
+		let buttonClasses = newRoom
+
 		newRoom.options.forEach(function(opt) {
 			if(!opt.requirements) {
-				options.append(`<button class="opt-btn opt-btn-js" data-destination="${opt.destination}">${opt.buttonText}</button>`);
+				options.append(`<button class="opt-btn js-opt-btn" data-destination="${opt.destination}">${opt.buttonText}</button>`);
 			}
 		});
 	}
 
+	function addHeader() {
+		const headerHtml = `
+			<h2 class="caps">Health: <span id="js-current-health">10</span>/<span id="js-max-health">10</span></h2>
+			<button class="opt-btn js-restart">Restart</button>
+		`;
+
+		$('header').html(headerHtml);
+	}
+
+	function resetGame() {
+		heading.addClass('caps');
+		heading.text(rooms.startScreen.heading);
+		copy.html('');
+		options.html('');
+
+		options.append(`<button class="opt-btn js-start cap">${rooms.startScreen.options[0].buttonText}</button>`);
+
+		removeHeader();
+	}
+
+	function removeHeader() {
+		$('header').html('');
+	}
+
 	$(document).on('click', '.js-start', function() {
-		moveRooms(firstRoom);
+		moveRooms(rooms['firstRoom']);
+		addHeader();
 	});
+
+	$(document).on('click', '.js-opt-btn', function() {
+		moveRooms(rooms[$(this).data('destination')]);
+	});
+
+	$(document).on('click', '.js-restart', function() {
+		resetGame();
+	});
+
+	// Adding and removing inventory items
+
+
+	// Change stats
+
+
+	// Check for saved values in local storage
 
 });
--- js/story-object.js
+++ js/story-object.js
@@ -1,21 +1,54 @@
-let firstRoom = {
-	heading: `You wake up.`,
-	copy: [
-		`A fine day for dragon killing! "Another day, another dragon," you always say. Although this would be the first dragon you killed, so how about "first dragon, first dollar?" This is probably the time to mention that you have no dollars. And no name, as of yet.`,
-		`What an exciting start!`
-	],
-	options: [
-		{
-			buttonText: `I know my name!`,
-			destination: 'nameEntry',
-		},
-		{
-			buttonText: `I think I know my name...?`,
-			destination: '',
-		},
-		{
-			buttonText: `I'd like to go back to sleep, please.`,
-			destination: '',
-		},
-	],
-};
+var rooms = {
+	startScreen: {
+		heading: 'Kill a Dragon',
+		copy: [],
+		options: [
+			{
+				buttonText: 'Begin',
+				destination: 'firstRoom',
+			}
+		]
+	},
+	firstRoom: {
+		heading: `You wake up.`,
+		copy: [
+			`A fine day for dragon killing! "Another day, another dragon," you always say. Although this would be the first dragon you killed, so how about "first dragon, first dollar?" This is probably the time to mention that you have no dollars. And nothing that can kill a dragon.`,
+			`What a promising start!`
+		],
+		options: [
+			{
+				buttonText: `Fight some goblins!`,
+				destination: 'fightGoblins',
+			},
+			{
+				buttonText: 'Search for tools.',
+				destination: 'searchTools',
+			},
+			{
+				buttonText: 'Fight the dragon!',
+				destination: 'confirmDragon',
+			},
+			{
+				buttonText: `I'd like to go back to sleep, please.`,
+				destination: 'backToSleep',
+			},
+		],
+	},
+	fightGoblins: {
+		heading: `Fightin' time.`,
+		copy: [
+			`What better way to start your day than fighting some goblins. Certainly not brushing your teeth!`,
+			`After a quick search, you discover a goblin camp. There are two goblins by a campfire engrossed in conversation.`
+		],
+		options: [
+			{
+				buttonText: 'Ambush them!',
+				destination: 'ambushGoblins',
+			},
+			{
+				buttonText: 'Eavesdrop on them.',
+				destination: 'eavesdropGoblins',
+			},
+		]
+	},
+}
